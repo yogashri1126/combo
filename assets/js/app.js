6e7b5d74d75d48bbcf806a99c645f6a3ed347397
yogashri1126 <ypradhan.pge@texas.edu>
2017-08-04 22:48:10 -0500

    figuring out YT

--- assets/js/app.js
+++ assets/js/app.js
@@ -218,6 +218,7 @@ $(document).on('click', '#newButton', function () {
 });
 
         // execute the request
+      function tplawesome(e,t){res=e;for(var n=0;n<t.length;n++){res=res.replace(/\{\{(.*?)\}\}/g,function(e,r){return t[n][r]})}return res}
        request.execute(function(response) {
           var results = response.result;
           $("#ytNew").html("");
