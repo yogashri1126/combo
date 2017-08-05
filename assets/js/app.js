a59c9b00320aa7da15899fda038dab0a649ca2a7
yogashri1126 <ypradhan.pge@texas.edu>
2017-08-04 23:45:04 -0500

    QC YT API with educational videos

--- assets/js/app.js
+++ assets/js/app.js
@@ -212,6 +212,7 @@ $(document).on('click', '#newButton', function () {
             q: encodeURIComponent($('input').val().trim()).replace(/%20/g, "+"),
             maxResults: 1,
             order: "relevance",
+            topicId: "/m/01k8wb"
             publishedAfter: "2015-01-01T00:00:00Z"
   // add iframe to html
   // $('#ytNew').html(iframe);
